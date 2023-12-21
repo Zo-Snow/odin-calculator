60389c41d88e463bd84634c6b910f889d6dca26c
Zo-Snow <zcr.snow@gmail.com>
2023-12-21 12:42:38 +0300

    Make numbers display and clear button work

--- index.js
+++ index.js
@@ -19,3 +19,33 @@ function calculator(operator, n1, n2) {
         return result; 
 }
 
+let string = "";
+
+function displayNums(className, num) {
+    document.querySelector(`.${className}`).addEventListener('click', () => {
+        string = string + num;
+        document.querySelector(".display").textContent = string;
+    })
+}
+
+displayNums("one", 1);
+displayNums("two", 2);
+displayNums("three", 3);
+displayNums("four", 4);
+displayNums("five", 5);
+displayNums("six", 6);
+displayNums("seven", 7);
+displayNums("eight", 8);
+displayNums("nine", 9);
+displayNums("zero", 0);
+
+
+document.querySelector(".clear").addEventListener('click', () => {
+    document.querySelector(".display").textContent = "";
+    string = "";
+})
+
+
+
+
+
